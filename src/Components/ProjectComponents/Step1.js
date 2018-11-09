fa60196ea2dfc2cb17922144a7c450ce732230fd
Chelsea Zhu <qianwei5668@gmail.com>
2018-11-09 13:02:45 -0800

    before using form

--- src/Components/ProjectComponents/Step1.js
+++ src/Components/ProjectComponents/Step1.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 
-
 class Step1 extends Component {
     constructor(props) {
         super(props);
@@ -11,18 +10,18 @@ class Step1 extends Component {
     render() {
         return (
             <div onClick = {this.props.onClick}>
-            <p>Step 1</p>
-            <Grid container spacing={12}>
-                <Grid item xs={3}>
+                <p>Step 1</p>
+                <Grid container spacing={12}>
+                    <Grid item xs={3}>
 
-                        {this.props.linkButtonToNextPage}
+                            {this.props.linkButtonToNextPage}
 
-                </Grid>         
-                <Grid item xs={1}>
+                    </Grid>         
+                    <Grid item xs={1}>
 
-                </Grid>                
-            </Grid>
-        </div>
+                    </Grid>                
+                </Grid>
+            </div>
         );
     }
 }
