6afe5d5e8bf925ed367f468758fc91c6fa39cd0e
Chelsea Zhu <qianwei5668@gmail.com>
2018-11-28 15:55:42 -0800

    delete steps and choose to use a whole form on a single page to simplify

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,50 +1,28 @@
 import React, {Component} from 'react';
-import axios from 'axios';
-
-import Step from './ProjectComponents/Step';
-import NavButtons from './NavButtons';
+import ImageUpload from './ProjectComponents/ImageUpload';
 
 
 class Project extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentButtonClicked: 0
+
     };
-    this.buttonClick = this.buttonClick.bind(this)
   }
 
-  buttonClick(event) {
-    this.setState({
-      currentButtonClicked: (this.state.currentButtonClicked + 1) % 3
-    })
-  }
+
 
   render() {
-    let nextButton = 
-      <div>
-          <NavButtons label = {'Next Step'} onClick = {this.buttonClick} />
-      </div>
-    let startOverButton = 
-    <div>
-        <NavButtons label = {'Start Over'} onClick = {this.buttonClick} />
-    </div>
 
-    if (this.state.currentButtonClicked !== 2) {
-      return (
-          <div>
-            <Step step = {this.state.currentButtonClicked + 1}  button = {nextButton} />
-          </div>
-        )
-      }
-    else {
+
       return (
         <div>
-          <Step step = {this.state.currentButtonClicked + 1}  button = {startOverButton} />
+          <ImageUpload />
         </div>
         )
-      }
+
     }
   }
   
 export default Project;
+
